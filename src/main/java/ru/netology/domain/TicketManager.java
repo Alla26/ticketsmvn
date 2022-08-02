da744d6c5e9b0d1a73cc2875285e37bc02eefc84
unknown <girlalla@yandex.ru>
2022-08-02 14:58:51 +0300

    Создала новый класс TicketByTravelTimeAscComparator, модифицировала метод findAll и скорректировала тесты

--- src/main/java/ru/netology/domain/TicketManager.java
+++ src/main/java/ru/netology/domain/TicketManager.java
@@ -3,6 +3,7 @@ package ru.netology.domain;
 import ru.netology.repository.TicketRepository;
 
 import java.util.Arrays;
+import java.util.Comparator;
 
 public class TicketManager {
 
@@ -22,7 +23,33 @@ public class TicketManager {
     }
 
 
-    public Ticket[] findAll(String from, String to) {
+    public Ticket[] findAll(String from, String to, Comparator<Ticket> comparator) {
+        Ticket[] result = new Ticket[0];               // тут будем хранить подошедшие запросу продукты
+        for (Ticket ticket : repo.showAll()) {
+            if (matches(ticket, from, to)) {
+                Ticket[] tmp = new Ticket[result.length + 1];
+                for (int i = 0; i < result.length; i++) {
+                    tmp[i] = result[i];
+                }
+                tmp[tmp.length - 1] = ticket;
+                result = tmp;
+            }
+            Arrays.sort(result, comparator);
+        }
+        return result;
+    }
+
+    public boolean matches(Ticket ticket, String from, String to) {
+        if (ticket.getFrom().contains(from)) {
+            return true;
+        } else if (ticket.getTo().contains(to)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public Ticket[] findAllWithoutComparator(String from, String to) {
         Ticket[] result = new Ticket[0];               // тут будем хранить подошедшие запросу продукты
         for (Ticket ticket : repo.showAll()) {
             if (matches(ticket, from, to)) {
@@ -36,16 +63,6 @@ public class TicketManager {
             Arrays.sort(result);
         }
         return result;
-    }
-
-    public boolean matches(Ticket ticket, String from, String to) {
-        if (ticket.getFrom().contains(from)) {
-            return true;
-        } else if (ticket.getTo().contains(to)) {
-            return true;
-        } else {
-            return false;
-        }
     }
 
 
--- src/test/java/ru/netology/domain/TicketManagerTest.java
+++ src/test/java/ru/netology/domain/TicketManagerTest.java
@@ -5,16 +5,20 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ru.netology.repository.TicketRepository;
 
+import java.util.Comparator;
+
 public class TicketManagerTest {
 
     TicketRepository repo = new TicketRepository();
     TicketManager manager = new TicketManager(repo);
+    TicketByTravelTimeAscComparator comparator = new TicketByTravelTimeAscComparator();
 
     Ticket ticket1 = new Ticket(1, 25_000, "DME", "KGD", 130);
-    Ticket ticket2 = new Ticket(2, 22_000, "AER", "VKO", 120);
+    Ticket ticket2 = new Ticket(2, 22_000, "AER", "VKO", 140);
     Ticket ticket3 = new Ticket(3, 23_000, "SVO", "LED", 120);
-    Ticket ticket4 = new Ticket(4, 23_000, "KZN", "GOJ", 120);
-    Ticket ticket5 = new Ticket(5, 21_000, "KZN", "VKO", 120);
+    Ticket ticket4 = new Ticket(4, 23_000, "KZN", "GOJ", 150);
+    Ticket ticket5 = new Ticket(5, 21_000, "KZN", "VKO", 145);
+    Ticket ticket6 = new Ticket(6, 21_000, "AER", "KGD", 145);
 
     @BeforeEach
     public void setup() {
@@ -23,14 +27,16 @@ public class TicketManagerTest {
         manager.add(ticket3);
         manager.add(ticket4);
         manager.add(ticket5);
+        manager.add(ticket6);
 
     }
 
     @Test
     public void shouldFindTicketsWhenNextLess() {
 
+
         Ticket[] expected = {ticket3, ticket1};
-        Ticket[] actual = manager.findAll("DME", "LED");
+        Ticket[] actual = manager.findAll("DME", "LED", comparator);
 
 
         Assertions.assertArrayEquals(expected, actual);
@@ -40,8 +46,8 @@ public class TicketManagerTest {
     @Test
     public void shouldFindTicketsWhenNextBigger() {
 
-        Ticket[] expected = {ticket2, ticket3};
-        Ticket[] actual = manager.findAll("AER", "LED");
+        Ticket[] expected = {ticket3, ticket2, ticket6};
+        Ticket[] actual = manager.findAll("AER", "LED", comparator);
 
 
         Assertions.assertArrayEquals(expected, actual);
@@ -52,7 +58,7 @@ public class TicketManagerTest {
     public void shouldFindTicketsWhenEqual() {
 
         Ticket[] expected = {ticket3, ticket4};
-        Ticket[] actual = manager.findAll("SVO", "GOJ");
+        Ticket[] actual = manager.findAll("SVO", "GOJ", comparator);
 
 
         Assertions.assertArrayEquals(expected, actual);
@@ -62,8 +68,19 @@ public class TicketManagerTest {
     @Test
     public void shouldFindTicketsMoreThanTwo() {
 
-        Ticket[] expected = {ticket5, ticket4, ticket1};
-        Ticket[] actual = manager.findAll("KZN", "KGD");
+        Ticket[] expected = {ticket1, ticket5, ticket6, ticket4};
+        Ticket[] actual = manager.findAll("KZN", "KGD", comparator);
+
+
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+
+    @Test
+    public void shouldFindTicketsWithoutComparator() {
+
+        Ticket[] expected = {ticket5, ticket6, ticket4, ticket1};
+        Ticket[] actual = manager.findAllWithoutComparator("KZN", "KGD");
 
 
         Assertions.assertArrayEquals(expected, actual);
